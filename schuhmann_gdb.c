3a7e91ed98529b4565b7fe58a68bf0d16f499f0f
William Schuhmann <wps2401@gmail.com>
2020-11-13 17:28:23 -0700

    Assignment 1 Files

---
+++ schuhmann_gdb.c
@@ -0,0 +1,30 @@
+/* schuhmann_gdb.c
+ * Author: William Schuhmann
+ * Created on: May 27, 2020
+ * ID: 1215016803
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+float frand();
+
+int main(int argc, char** argv)
+{
+  srand(999);
+  int i;
+  for(i = 1; i <= 100; i++)
+    {
+      int value = ((int)frand() * 1000000);
+      printf("%6d ",value);
+      if(i % 20 == 0) printf("\n");
+    }
+  printf("\n");
+  return 0;
+}
+
+float frand()
+{
+  return ((float)rand())/RAND_MAX;
+}
